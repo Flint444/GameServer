09e0d6803bd3a337c442bc4c63d1a7b563757582
Flint444 <maksimgrabauskas@gmail.com>
2022-10-22 19:44:32 +0200

    8.2 #Add prize in model Achievements

--- server/achievements/models.py
+++ server/achievements/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Achievements(models.Model):
     title = models.CharField(max_length=255, unique=True)
+    prize = models.CharField(max_length=255)
     image = models.ImageField(upload_to = '')
 
     def __str__(self):
